eb1d7dcac827630de93895cfbf213044986d890c
Martin N <m4rtini89@gmail.com>
2021-11-14 22:58:49 +0100

    rewrite begins

--- source/ballchasing/APIDataClasses.cpp
+++ source/ballchasing/APIDataClasses.cpp
@@ -1,209 +1,71 @@
 #include "pch.h"
-#include "../json/json.hpp"
-#include "APIDataClasses.h"
-
-#define JOPTIONAL(var) if (j.find(#var) != j.end()){j.at(#var).get_to(p.var);}
-#define JOPTIONAL2(var, var2) if (j.find(#var) != j.end()){j.at(#var).get_to(p.var2);}
-#define J(var) j.at(#var).get_to(p.var);
-#define J2(var, var2) j.at(#var).get_to(p.var2);
-
-
-void from_json(const nlohmann::json & j, GetReplayGroupsResponseData & p) {
-	JOPTIONAL(next);
-	JOPTIONAL(list)
-}
-
-void from_json(const nlohmann::json& j, ReplayData& p) {
-	JOPTIONAL(replay_title);
-	JOPTIONAL2(title, replay_title);
-	JOPTIONAL(status);
-	JOPTIONAL(id);
-	JOPTIONAL(link);
-	JOPTIONAL(blue);
-	JOPTIONAL(orange);
-}
-
-void from_json(const nlohmann::json& j, GetReplaysResponse& p) {
-	JOPTIONAL(count);
-	JOPTIONAL2(list, replays);
-}
-
-void from_json(const nlohmann::json& j, Team& p)
-{
-	JOPTIONAL(name);
-	JOPTIONAL(goals);
-	JOPTIONAL(players);
-}
-
-void from_json(const nlohmann::json& j, Team::Player& p)
-{
-	JOPTIONAL(name);
-	JOPTIONAL(score);
-	JOPTIONAL(id);
-	JOPTIONAL(stats);
-	JOPTIONAL(camera);
-}
-
-void from_json(const nlohmann::json& j, Team::Player::Id& p)
-{
-	JOPTIONAL(id);
-	JOPTIONAL(platform);
-}
-
-void from_json(const nlohmann::json& j, PlayerStats& p)
-{
-	JOPTIONAL(core);
-	JOPTIONAL(boost);
-	JOPTIONAL(movement);
-	JOPTIONAL(positioning);
-	JOPTIONAL(demo);
-}
-
-void from_json(const nlohmann::json& j, CoreStats& p)
-{
-	JOPTIONAL(shots);
-	JOPTIONAL(shots_against);
-	JOPTIONAL(goals);
-	JOPTIONAL(goals_against);
-	JOPTIONAL(saves);
-	JOPTIONAL(assists);
-	JOPTIONAL(score);
-	JOPTIONAL(mvp);
-	JOPTIONAL(shooting_percentage);
-}
-
-void from_json(const nlohmann::json& j, BoostStats& p)
-{
-	JOPTIONAL(bpm);
-	JOPTIONAL(bcpm);
-	JOPTIONAL(avg_amount);
-	JOPTIONAL(amount_collected);
-	JOPTIONAL(amount_stolen);
-	JOPTIONAL(amount_collected_big);
-	JOPTIONAL(amount_stolen_big);
-	JOPTIONAL(amount_collected_small);
-	JOPTIONAL(amount_stolen_small);
-	JOPTIONAL(count_collected_big);
-	JOPTIONAL(count_stolen_big);
-	JOPTIONAL(count_collected_small);
-	JOPTIONAL(count_stolen_small);
-	JOPTIONAL(amount_overfill);
-	JOPTIONAL(amount_overfill_stolen);
-	JOPTIONAL(amount_used_while_supersonic);
-	JOPTIONAL(time_zero_boost);
-	JOPTIONAL(percent_zero_boost);
-	JOPTIONAL(time_full_boost);
-	JOPTIONAL(percent_full_boost);
-	JOPTIONAL(time_boost_0_25);
-	JOPTIONAL(time_boost_25_50);
-	JOPTIONAL(time_boost_50_75);
-	JOPTIONAL(time_boost_75_100);
-	JOPTIONAL(percent_boost_0_25);
-	JOPTIONAL(percent_boost_25_50);
-	JOPTIONAL(percent_boost_50_75);
-	JOPTIONAL(percent_boost_75_100);
-}
-
-void from_json(const nlohmann::json& j, MovementStats& p)
-{
-	JOPTIONAL(avg_speed);
-	JOPTIONAL(total_distance);
-	JOPTIONAL(time_supersonic_speed);
-	JOPTIONAL(time_boost_speed);
-	JOPTIONAL(time_slow_speed);
-	JOPTIONAL(time_ground);
-	JOPTIONAL(time_low_air);
-	JOPTIONAL(time_high_air);
-	JOPTIONAL(time_powerslide);
-	JOPTIONAL(count_powerslide);
-	JOPTIONAL(avg_powerslide_duration);
-	JOPTIONAL(avg_speed_percentage);
-	JOPTIONAL(percent_slow_speed);
-	JOPTIONAL(percent_boost_speed);
-	JOPTIONAL(percent_supersonic_speed);
-	JOPTIONAL(percent_ground);
-	JOPTIONAL(percent_low_air);
-	JOPTIONAL(percent_high_air);
-}
-
-void from_json(const nlohmann::json& j, PositioningStats& p)
-{
-	JOPTIONAL(avg_distance_to_ball);
-	JOPTIONAL(avg_distance_to_ball_possession);
-	JOPTIONAL(avg_distance_to_ball_no_possession);
-	JOPTIONAL(avg_distance_to_mates);
-	JOPTIONAL(time_defensive_third);
-	JOPTIONAL(time_neutral_third);
-	JOPTIONAL(time_offensive_third);
-	JOPTIONAL(time_defensive_half);
-	JOPTIONAL(time_offensive_half);
-	JOPTIONAL(time_behind_ball);
-	JOPTIONAL(time_infront_ball);
-	JOPTIONAL(time_most_back);
-	JOPTIONAL(time_most_forward);
-	JOPTIONAL(goals_against_while_last_defender);
-	JOPTIONAL(time_closest_to_ball);
-	JOPTIONAL(time_farthest_from_ball);
-	JOPTIONAL(percent_defensive_third);
-	JOPTIONAL(percent_offensive_third);
-	JOPTIONAL(percent_neutral_third);
-	JOPTIONAL(percent_defensive_half);
-	JOPTIONAL(percent_offensive_half);
-	JOPTIONAL(percent_behind_ball);
-	JOPTIONAL(percent_infront_ball);
-	JOPTIONAL(percent_most_back);
-	JOPTIONAL(percent_most_forward);
-	JOPTIONAL(percent_closest_to_ball);
-	JOPTIONAL(percent_farthest_from_ball);
-}
-
-void from_json(const nlohmann::json& j, DemoStats& p)
-{
-	JOPTIONAL(inflicted);
-	JOPTIONAL(taken);
-}
-
-void from_json(const nlohmann::json& j, CameraSettings& p)
-{
-	JOPTIONAL(fov);
-	JOPTIONAL(height);
-	JOPTIONAL(pitch);
-	JOPTIONAL(distance);
-	JOPTIONAL(stiffness);
-	JOPTIONAL(swivel_speed);
-	JOPTIONAL(transition_speed);
-}
-
-void from_json(const nlohmann::json& j, GroupData& p)
-{
-	JOPTIONAL(id);
-	JOPTIONAL(link);
-	JOPTIONAL(name);
-	JOPTIONAL(created);
-	JOPTIONAL(status);
-	JOPTIONAL(shared);
-	JOPTIONAL(players);
-}
-
-void from_json(const nlohmann::json& j, GroupPlayer& p)
-{
-	JOPTIONAL(platform);
-	JOPTIONAL(id);
-	JOPTIONAL(name);
-	JOPTIONAL(team);
-	JOPTIONAL2(cumulative, cumulative_stats);
-	JOPTIONAL2(game_average, stats);
-}
-
-void from_json(const nlohmann::json& j, CumulativePlayerStats& p)
-{
-	from_json(j, (PlayerStats&)p);
-	JOPTIONAL(games);
-	JOPTIONAL(wins);
-	JOPTIONAL(win_percentage);
-	JOPTIONAL(play_duration);
-}
-
-
-#undef J
-#undef JOPTIONAL
+//#include "../json/json.hpp"
+//#include "APIDataClasses.h"
+//
+//#define JOPTIONAL(var) if (j.find(#var) != j.end()){j.at(#var).get_to(p.var);}
+//#define JOPTIONAL2(var, var2) if (j.find(#var) != j.end()){j.at(#var).get_to(p.var2);}
+//#define J(var) j.at(#var).get_to(p.var);
+//#define J2(var, var2) j.at(#var).get_to(p.var2);
+//
+//
+//void from_json(const nlohmann::json & j, GetReplayGroupsResponseData & p) {
+//	JOPTIONAL(next)
+//	JOPTIONAL(list)
+//}
+//
+//void from_json(const nlohmann::json& j, ReplayData& p) {
+//	JOPTIONAL(replay_title)
+//	JOPTIONAL2(title, replay_title)
+//	JOPTIONAL(status)
+//	JOPTIONAL(id)
+//	JOPTIONAL(link)
+//	JOPTIONAL(blue)
+//	JOPTIONAL(orange)
+//}
+//
+//void from_json(const nlohmann::json& j, GetReplaysResponse& p) {
+//	JOPTIONAL(count)
+//	JOPTIONAL2(list, replays)
+//}
+//
+//
+////void from_json(const nlohmann::json& j, Team::Player& p)
+////{
+////	JOPTIONAL(name)
+////	JOPTIONAL(score)
+////	JOPTIONAL(id)
+////	JOPTIONAL(stats)
+////	JOPTIONAL(camera)
+////}
+////
+////void from_json(const nlohmann::json& j, Team::Player::Id& p)
+////{
+////	JOPTIONAL(id)
+////	JOPTIONAL(platform)
+////}
+//
+//void from_json(const nlohmann::json& j, GroupData& p)
+//{
+//	JOPTIONAL(id)
+//	JOPTIONAL(link)
+//	JOPTIONAL(name)
+//	JOPTIONAL(created)
+//	JOPTIONAL(status)
+//	JOPTIONAL(shared)
+//	JOPTIONAL(players)
+//}
+//
+//void from_json(const nlohmann::json& j, GroupPlayer& p)
+//{
+//	JOPTIONAL(platform)
+//	JOPTIONAL(id)
+//	JOPTIONAL(name)
+//	JOPTIONAL(team)
+//	JOPTIONAL2(cumulative, cumulative_stats)
+//	JOPTIONAL2(game_average, stats)
+//}
+//
+//
+//
+//#undef J
+//#undef JOPTIONAL
--- source/ballchasing/APIDataClasses.h
+++ source/ballchasing/APIDataClasses.h
@@ -3,14 +3,16 @@
 #include <vector>
 #include "../json/json.hpp"
 
-enum class RequestState {
+enum class RequestState
+{
 	UNKNOWN = 0,
 	REQUESTED = 1,
 	FAILED = 2,
 	SUCCESS = 3
 };
 
-struct CoreStats {
+struct CoreStats
+{
 	float shots = 0;
 	float shots_against = 0;
 	float goals = 0;
@@ -22,7 +24,11 @@ struct CoreStats {
 	float shooting_percentage = 0;
 };
 
-struct BoostStats {
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(CoreStats, shots, shots_against, goals, goals_against, saves, assists,
+                                            score, mvp, shooting_percentage)
+
+struct BoostStats
+{
 	float bpm = 0;
 	float bcpm = 0;
 	float avg_amount = 0;
@@ -53,7 +59,17 @@ struct BoostStats {
 	float percent_boost_75_100 = 0;
 };
 
-struct MovementStats {
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(BoostStats, bpm, bcpm, avg_amount, amount_collected, amount_stolen,
+                                            amount_collected_big, amount_stolen_big, amount_collected_small,
+                                            amount_stolen_small, count_collected_big, count_stolen_big,
+                                            count_collected_small, count_stolen_small, amount_overfill,
+                                            amount_overfill_stolen, amount_used_while_supersonic, time_zero_boost,
+                                            percent_zero_boost, time_full_boost, percent_full_boost, time_boost_0_25,
+                                            time_boost_25_50, time_boost_50_75, time_boost_75_100, percent_boost_0_25,
+                                            percent_boost_25_50, percent_boost_50_75, percent_boost_75_100);
+
+struct MovementStats
+{
 	float avg_speed = 0;
 	float total_distance = 0;
 	float time_supersonic_speed = 0;
@@ -74,7 +90,15 @@ struct MovementStats {
 	float percent_high_air = 0;
 };
 
-struct PositioningStats {
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(MovementStats, avg_speed, total_distance, time_supersonic_speed,
+                                            time_boost_speed, time_slow_speed, time_ground, time_low_air, time_high_air,
+                                            time_powerslide, count_powerslide, avg_powerslide_duration,
+                                            avg_speed_percentage, percent_slow_speed, percent_boost_speed,
+                                            percent_supersonic_speed, percent_ground, percent_low_air,
+                                            percent_high_air);
+
+struct PositioningStats
+{
 	float avg_distance_to_ball = 0;
 	float avg_distance_to_ball_possession = 0;
 	float avg_distance_to_ball_no_possession = 0;
@@ -104,12 +128,27 @@ struct PositioningStats {
 	float percent_farthest_from_ball = 0;
 };
 
-struct DemoStats {
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(PositioningStats, avg_distance_to_ball, avg_distance_to_ball_possession,
+                                            avg_distance_to_ball_no_possession, avg_distance_to_mates,
+                                            time_defensive_third, time_neutral_third, time_offensive_third,
+                                            time_defensive_half, time_offensive_half, time_behind_ball,
+                                            time_infront_ball, time_most_back, time_most_forward,
+                                            goals_against_while_last_defender, time_closest_to_ball,
+                                            time_farthest_from_ball, percent_defensive_third, percent_offensive_third,
+                                            percent_neutral_third, percent_defensive_half, percent_offensive_half,
+                                            percent_behind_ball, percent_infront_ball, percent_most_back,
+                                            percent_most_forward, percent_closest_to_ball, percent_farthest_from_ball);
+
+struct DemoStats
+{
 	float inflicted = 0;
 	float taken = 0;
 };
 
-struct CameraSettings {
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(DemoStats, inflicted, taken);
+
+struct CameraSettings
+{
 	int fov;
 	int height;
 	int pitch;
@@ -118,6 +157,9 @@ struct CameraSettings {
 	float swivel_speed;
 	float transition_speed;
 };
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(CameraSettings, fov, height, pitch, distance, stiffness, swivel_speed,
+                                            transition_speed);
 
 struct PlayerStats
 {
@@ -128,43 +170,55 @@ struct PlayerStats
 	DemoStats demo;
 };
 
-
-
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(PlayerStats, core, boost, movement, positioning, demo);
 
 
 struct CumulativePlayerStats : PlayerStats
 {
-	int		games;
-	int		wins;
-	float	win_percentage;
-	int		play_duration;
+	int games;
+	int wins;
+	float win_percentage;
+	int play_duration;
 };
 
-struct BaseStatPlayer {
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(CumulativePlayerStats, games, wins, win_percentage, play_duration);
+
+struct BaseStatPlayer
+{
 	std::string name;
 	PlayerStats stats;
 	CumulativePlayerStats cumulative_stats;
 	CameraSettings camera;
 };
 
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(BaseStatPlayer, name, stats, cumulative_stats, camera);
+
 struct Team
 {
 	std::string name;
 	int goals = 0;
-	struct Player : BaseStatPlayer {
+
+	struct Player : BaseStatPlayer
+	{
 		//std::string name;
-		struct Id {
+		struct Id
+		{
 			std::string platform;
 			std::string id;
 		};
+
 		Id id;
 		int score;
-		
-		//PlayerStats stats;
 
+		//PlayerStats stats;
 	};
+
 	std::vector<Player> players;
 };
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(Team::Player::Id, id, platform);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(Team::Player, id, score);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(Team, name, goals, players);
 
 struct ReplayData
 {
@@ -177,17 +231,16 @@ struct ReplayData
 	Team orange;
 };
 
-struct GroupPlayer:  BaseStatPlayer
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(ReplayData, replay_title, id, link, status, blue, orange);
+
+struct GroupPlayer : BaseStatPlayer
 {
 	std::string platform;
 	std::string id;
-	//std::string name;
 	std::string team;
-	//CumulativePlayerStats cumulative;
-	//PlayerStats game_average;
-
 };
 
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(GroupPlayer, platform, id, team);
 
 
 struct GroupData
@@ -198,13 +251,13 @@ struct GroupData
 	std::string created;
 	std::string status;
 	bool shared;
-
 	std::vector<GroupPlayer> players;
-
 	RequestState subGroupsRequested = RequestState::UNKNOWN;
 	std::vector<std::string> subgroups;
 	std::vector<ReplayData> groupReplays;
 };
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(GroupData, id, link, name, created, status, shared, players);
 
 struct GetReplaysResponse
 {
@@ -212,30 +265,33 @@ struct GetReplaysResponse
 	std::vector<ReplayData> replays;
 };
 
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(GetReplaysResponse, count, replays)
+
 struct GetReplayGroupsResponseData
 {
 	std::string next;
 	std::vector<GroupData> list;
 };
 
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_OPTIONAL(GetReplayGroupsResponseData, next, list)
 
-void from_json(const nlohmann::json& j, ReplayData& p);
-void from_json(const nlohmann::json& j, GetReplaysResponse& p);
-void from_json(const nlohmann::json& j, Team& p);
-void from_json(const nlohmann::json& j, Team::Player& p);
-void from_json(const nlohmann::json& j, Team::Player::Id& p);
-void from_json(const nlohmann::json& j, PlayerStats& p);
-
-
-void from_json(const nlohmann::json& j, CoreStats& p);
-void from_json(const nlohmann::json& j, BoostStats& p);
-void from_json(const nlohmann::json& j, MovementStats& p);
-void from_json(const nlohmann::json& j, PositioningStats& p);
-void from_json(const nlohmann::json& j, DemoStats& p);
-void from_json(const nlohmann::json& j, CameraSettings& p);
-
-void from_json(const nlohmann::json& j, GetReplayGroupsResponseData& p);
-void from_json(const nlohmann::json& j, GroupData& p);
-void from_json(const nlohmann::json& j, GroupPlayer& p);
-void from_json(const nlohmann::json& j, CumulativePlayerStats& p);
-
+//
+//void from_json(const nlohmann::json& j, ReplayData& p);
+//void from_json(const nlohmann::json& j, GetReplaysResponse& p);
+//void from_json(const nlohmann::json& j, Team& p);
+//void from_json(const nlohmann::json& j, Team::Player& p);
+//void from_json(const nlohmann::json& j, Team::Player::Id& p);
+//void from_json(const nlohmann::json& j, PlayerStats& p);
+//
+//
+//void from_json(const nlohmann::json& j, CoreStats& p);
+//void from_json(const nlohmann::json& j, BoostStats& p);
+//void from_json(const nlohmann::json& j, MovementStats& p);
+//void from_json(const nlohmann::json& j, PositioningStats& p);
+//void from_json(const nlohmann::json& j, DemoStats& p);
+//void from_json(const nlohmann::json& j, CameraSettings& p);
+//
+//void from_json(const nlohmann::json& j, GetReplayGroupsResponseData& p);
+//void from_json(const nlohmann::json& j, GroupData& p);
+//void from_json(const nlohmann::json& j, GroupPlayer& p);
+//void from_json(const nlohmann::json& j, CumulativePlayerStats& p);
